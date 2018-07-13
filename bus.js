c8711e367a78589827ac929ee7275aa060fd4b26
Yann Cedric <yann_cedric.ngadeu_molapie@bell.ca>
2018-07-13 16:09:38 -0400

    Added middleware logfile

--- bus.js
+++ bus.js
@@ -1,9 +1,21 @@
 const io = require('socket.io')()
 const argv = require('yargs').argv
-const logger = require('fancy-log')
+const fancylog = require('fancy-log')
 const someWord = require('random-words')
+const fs = require('fs')
 
-const {p = 8888 , port = p, n=someWord(), name=n} = process.env.ENV === 'docker' ? process.env : argv
+const {logfile='./logs.txt' ,p = 8888 , port = p, n=someWord(), name=n} = process.env.ENV === 'docker' ? process.env : argv
+
+const fileLogger = fs.createWriteStream(logfile, {
+    flags: 'a' // 'a' means appending (old data will be preserved)
+})
+
+// Logger middleware to write into text file
+let logger = (log) => {
+    const msg = log+'\n'
+    fileLogger.write(msg)
+    fancylog(msg)
+}
 
 const events = require('./events')
 
@@ -25,5 +37,7 @@ io.on('connection', function(client){
 
 
 
+// const success = io.listen(port)
+// if (success) logger(`event bus ${name.toUpperCase()} started on ${port}`)
 const success = io.listen(port)
-if (success) logger(`event bus ${name.toUpperCase()} started on ${port}`)
+logger(`event bus ${name.toUpperCase()} started on ${port}`)
--- test.js
+++ test.js
@@ -1,11 +1,16 @@
-BusClient = require('./client')
+BusClient = require('./BusClient')
 const events = require('./events')
 
-let c1 = new BusClient('http://localhost:8000')
+let c1 = new BusClient('http://localhost:8888')
 c1.login({username: 'yann', password: 'lul'})
+let ctr = 0
 
-let c2 = new BusClient('http://localhost:8000')
+setInterval(()=> {
+    c1.login({username: 'bigsott', password: ctr++})
+}, 2000)
+
+let c2 = new BusClient('http://localhost:8888')
 c2.listen(events.login, (data)=> {
     console.log('oh shit!',data)
-    c2.connection.disconnect()
+    // c2.connection.disconnect()
 })
